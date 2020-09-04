6b4836c2aa595c32da5581101c44f6e0c510f806
Subba <Subba@gmail.com>
2020-09-03 23:26:09 -0500

    removed contact.html + site.js

--- site.js
+++ site.js
@@ -1,31 +1,24 @@
-// const el = document.querySelector("#module");
-
-// el.addEventListener("mousemove", (e) => {
-//   el.style.backgroundPositionX = -e.offsetX + "px";
-//   el.style.backgroundPositionY = -e.offsetY + "px";
-// });
-
-//   $(function() {
+// $(function() {
 //     var $window = $(window),
-//     $body = $('body');
-//     $window.scroll(function () {
-//        if ($(this).scrollTop() > 300) {
-//           $body.addClass('changeColor')
-//        }
-//        if ($(this).scrollTop() < 300) {
-//           $body.removeClass('changeColor')
-//        }
-//        if ($(this).scrollTop() > 600) {
-//         $body.addClass('changeColor2')
-//       }
-//       if ($(this).scrollTop() < 600) {
-//           $body.removeClass('changeColor2')
-//       }
-//       if ($(this).scrollTop() > 1200) {
-//         $body.addClass('changeColor3')
-//       }
-//       if ($(this).scrollTop() < 1200) {
-//           $body.removeClass('changeColor3')
-//       }
+//         $body = $('body');
+//     $window.scroll(function() {
+//         if ($(this).scrollTop() > 300) {
+//             $body.addClass('changeColor')
+//         }
+//         if ($(this).scrollTop() < 300) {
+//             $body.removeClass('changeColor')
+//         }
+//         if ($(this).scrollTop() > 600) {
+//             $body.addClass('changeColor2')
+//         }
+//         if ($(this).scrollTop() < 600) {
+//             $body.removeClass('changeColor2')
+//         }
+//         if ($(this).scrollTop() > 1200) {
+//             $body.addClass('changeColor3')
+//         }
+//         if ($(this).scrollTop() < 1200) {
+//             $body.removeClass('changeColor3')
+//         }
 //     });
-//  });
+// });
